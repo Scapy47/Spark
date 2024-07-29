91347de7d2b07caf475cca2314bd478589da0b1a
scapy_47 <scapy47.5@proton.me>
2024-07-29 13:48:22 +0530

    DIALOG IS NOW READY WORKING AND BETTER

--- src/components/DiaLog.jsx
+++ src/components/DiaLog.jsx
@@ -4,23 +4,34 @@ import PropTypes from 'prop-types'
 
 const DiaLog = ({ children, open }) => {
     const dialogRef = useRef(null)
-    const toggle = () => {
-        dialogRef.current.hasAttribute("open") ? dialogRef.current.close()
-            : dialogRef.current.showModal()
-    }
+    // const toggle = () => {
+    //     dialogRef.current.hasAttribute("open") ? dialogRef.current.close()
+    //         : dialogRef.current.showModal()
+    // }
 
     useEffect(() => {
         open ? dialogRef.current.showModal() : dialogRef.current.close()
     }, [open])
 
     return (
-        <dialog className="rounded-xl p-2 select-none w-11/12 backdrop:bg-black backdrop:opacity-45"
+        <dialog className="select-none p-2 border border-black rounded-2xl backdrop-blur-3xl w-11/12 backdrop:backdrop-blur-md bg-transparent"
             ref={dialogRef}
-            onClick={(e) => e.currentTarget === e.target && toggle()}
+            onClick={(e) => {
+                const Dimensions = e.currentTarget.getBoundingClientRect()
+                if (
+                    e.clientX < Dimensions.left ||
+                    e.clientX > Dimensions.right ||
+                    e.clientY < Dimensions.top ||
+                    e.clientY > Dimensions.bottom
+                    // TODO ; how this is working 
+                ) {
+                    e.target.close()
+                }
+                // console.log(e.currentTarget.getBoundingClientRect())
+                // console.log(`x: ${e.clientX} Y: ${e.clientY}`)
+            }}
         >
-            <div>
-                {children}
-            </div>
+            {children}
         </dialog>
     )
 }

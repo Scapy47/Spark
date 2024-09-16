1b1113179da224eb4d28f2afb0e0e3a485a3cbcc
scapy47 <scapy47>
2024-09-17 00:43:46 +0530

    Hachtxt is done

--- src/components/Hacktxt.jsx
+++ src/components/Hacktxt.jsx
@@ -1,25 +1,40 @@
-import PropTypes from 'prop-types'
-import { useRef, useState } from 'react';
+import { useReducer, useRef, useState } from 'react';
+import PropTypes from 'prop-types';
 
-const Hacktxt = ({ children ,className }) => {
-    const chars = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
-    const inTXT = useRef(children)
-    const [txt, setTxt] = useState(inTXT)
+const Hacktxt = ({ children = "ERROR", className }) => {
+    const inTXT = useRef(children);
+    const [txt, setTxt] = useState(children);
 
-    function animate() {
-        
+    const randomChar = () => {
+        const chars = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
+        return chars[Math.round(Math.random() * chars.length)];
     }
 
-    return (
-        <>
-        <p onMouseEnter={animate} className={className}>{txt}</p>
-        </>
-    )
+    const runCount = useRef(0)
+    function animate() {
+
+        const intervalID = setInterval(() => {
+
+            setTxt(inTXT.current.split("").map((e, index) => {
+                if (index < runCount.current) return e
+                return randomChar()
+            }).join(""))
+
+            if (runCount.current >= inTXT.current.length) {
+                runCount.current = 0
+                clearInterval(intervalID)
+            }
+            runCount.current += 1 / 3
+            console.log(runCount.current)
+        }, 30)
+    }
+
+
+    return <p onMouseOver={animate} className={className}>{txt}</p>
 }
 
 Hacktxt.propTypes = {
-    children: PropTypes.any,
-    className: PropTypes.string,
+    children: PropTypes.string,
+    className: PropTypes.any,
 }
-
-export default Hacktxt 
+export default Hacktxt;
